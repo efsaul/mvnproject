b335b605a80b631fa09a6fea6331a4cd7dccf4bf
Francis Saul <62688787+efsaul@users.noreply.github.com>
2024-04-25 21:21:02 +0800

    Update UITest.java

--- src/test/java/mavenforjenkins/UITest.java
+++ src/test/java/mavenforjenkins/UITest.java
@@ -3,6 +3,7 @@ package mavenforjenkins;
 import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
